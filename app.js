5cd293735ca1920c118c9a2886285db9246c078e
Nick Besse <nbesse@mail.sfsu.edu>
2019-02-13 20:50:14 -0800

    Two apps are working

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/first', (req, res) => res.send('Hey, it works!'));
+app.get('/second', (req, res) => res.send ('Does this make me a software developer?'));
+app.get('/third', (req, res) => {
+    console.log(req.query);
+    res.send('My name is  ' + req.query.name)
+});
+
+app.get('/network', (req, res) => {
+    axios.get('http://localhost:3001/world')
+    .then((networkResponse) => {
+        res.send(networkResponse.data);
+    })
+    .catch(() => {
+        res.send(':( did not work.');
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
